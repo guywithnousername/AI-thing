b032503015e3dd0948cdcaffe9288916a19f9be1
Minglang Du <minglangdu@gmail.com>
2024-09-04 21:00:37 -0700

    add sklearn

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
 
-print("hello world!")
+loan_file_path = "loan.csv"
+loan_data = pd.read_csv(loan_file_path)
+y = loan_data.loan_status
+
+features = ['age', 'gender', 'occupation', 'education_level', 'marital_status', 'income', 'credit_score']
+X = loan_data[features]
+
+loan_model = DecisionTreeRegressor(random_state = 1)
+
+loan_model.fit(X, y)
